50f3340bd0678a110310daf5d8fd808c4cf07657
Pantz71 <nonameblank10@gmail.com>
2025-12-19 20:06:57 +0700

    idk

---
+++ src/main/java/pantz/mod/core/other/PMEvents.java
@@ -0,0 +1,8 @@
+package pantz.mod.core.other;
+
+import net.minecraftforge.fml.common.Mod;
+import pantz.mod.core.PantzMod;
+
+@Mod.EventBusSubscriber(modid = PantzMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class PMEvents {
+}
