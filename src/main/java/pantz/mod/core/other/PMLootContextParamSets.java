5d03d121854d63bc66d861c637e26f1f1e1c67e7
Pantz71 <nonameblank10@gmail.com>
2026-02-14 18:16:24 +0700

    stuff

--- src/main/java/pantz/mod/core/other/PMLootContextParamSets.java
+++ src/main/java/pantz/mod/core/other/PMLootContextParamSets.java
@@ -6,5 +6,6 @@ import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 public class PMLootContextParamSets {
     public static final LootContextParamSet RED_ENVELOPE = LootContextParamSet.builder()
             .required(LootContextParams.THIS_ENTITY)
+            .required(LootContextParams.ORIGIN)
             .build();
 }
